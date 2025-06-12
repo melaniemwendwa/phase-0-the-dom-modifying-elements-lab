797ec59ae8fb12f6cddf142f5bc5212bb1aded43
melanie mwendwa <mwendwathuranira@gmail.com>
2025-06-12 16:55:04 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+// Remove the <main id="main"> element
+const main = document.getElementById('main');
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id to 'victory'
+newHeader.id = 'victory';
+
+// Set the text inside the <h1>
+newHeader.textContent = 'Mwendwa Thuranira is the champion';
+
